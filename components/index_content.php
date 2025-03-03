6ff132d0f0067058872c64991907ff6de0f2e8b4
leo1423 <leoductan.lk@gmail.com>
2025-03-03 22:43:16 +0100

    homepage

---
+++ components/index_content.php
@@ -0,0 +1,11 @@
+<div class="content-sections">
+    <div class="hero_section">
+        <img src="../assets/images/index_image1.jpg" alt="Hero Image">
+    </div>
+    <div class="white_section">
+        <h2>Kategorieübersicht</h2>
+    </div>
+    <div class="dark_section">
+        <h2>Neuzugang</h2>
+    </div>
+</div>
--- pages/index.php
+++ pages/index.php
@@ -3,19 +3,20 @@
   <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/style_index.css" />
     <title>Sigmacars | Startseite</title>
   </head>
   <body>
     <?php include '../components/header.php'; ?>
-     <main class="main_content">
-      
-     <?php include '../components/filterform_index.php'; ?> 
-      <!-- Platzhalter für den weiteren Inhalt -->
-      <div class="content_section">
-          <p>Hier kommen die anderen drei Abschnitte hin...</p>
-          <div style="height: 1500px;"></div> <!-- Zum Testen des Scrollens -->
+    <?php include '../components/filterform_index.php'; ?> 
+    <?php include '../components/index_content.php'; ?>
+
+    <!-- <main class="main_content">
+       Platzhalter für den weiteren Inhalt 
+      <div class="content_section"> 
+           <div style="height: 1500px;"></div> Zum Testen des Scrollens
       </div>
-    </main>
+    </main> -->
     <?php include '../components/footer.php'; ?>
     <script src="../js/script.js"></script>
   </body>
