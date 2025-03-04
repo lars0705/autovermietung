2511ee382cf636ed1093ce5835f80ff1703fcfdd
lars0705 <lars.kehrhahn@icloud.com>
2025-03-04 14:02:02 +0100

    Remove unused CSS file and update navigation button classes for consistency

--- components/header.php
+++ components/header.php
@@ -12,12 +12,12 @@ $istAngemeldet = isset($_SESSION['user']); // Prüfen, ob der Nutzer angemeldet 
     </div>
     <nav class="nav_buttons">
     <?php if (isset($_SESSION["user_id"]) || isset($_COOKIE["user_id"])): ?>
-        <a href="../pages/profile.php" class="button">Profil</a>
-        <a href="../pages/bookings.php" class="button">Bestellungen</a>
-        <a href="../pages/logout.php" class="button">Abmelden</a>
+        <a href="../pages/profile.php" class="nav_button">Profil</a>
+        <a href="../pages/bookings.php" class="nav_button">Bestellungen</a>
+        <a href="../pages/logout.php" class="nav_button">Abmelden</a>
     <?php else: ?>
-        <a href="../pages/login.php" class="button">Anmelden</a>
-        <a href="../pages/register.php" class="button">Registrieren</a>
+        <a href="../pages/login.php" class="nav_button">Anmelden</a>
+        <a href="../pages/register.php" class="nav_button">Registrieren</a>
     <?php endif; ?>
     </nav>
 </header>
