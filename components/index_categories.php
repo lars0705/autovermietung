4b08c68160de9007b8d916cc84a409aa5fb08e1f
leo1423 <leoductan.lk@gmail.com>
2025-03-07 01:39:11 +0100

    merging

--- components/index_categories.php
+++ components/index_categories.php
@@ -12,11 +12,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const pickupDate = document.getElementById("pickup_date");
     const returnDate = document.getElementById("return_date");
-<<<<<<< HEAD:components/index_content.php
     const locationInput = document.getElementById("location"); 
-=======
-    const locationInput = document.getElementById("location");
->>>>>>> 7c85a137021f772a6e3ab1733566faeacfc48279:components/index_categories.php
     const errorMessage = document.getElementById("error_message");
     const categoryCards = document.querySelectorAll(".category_card");
 
