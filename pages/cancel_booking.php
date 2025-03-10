c55c6ec73121509af3405a91e3e489dce5f1d430
lars0705 <lars.kehrhahn@icloud.com>
2025-03-10 09:54:20 +0100

    bruteforce gefixt

--- pages/cancel_booking.php
+++ pages/cancel_booking.php
@@ -3,7 +3,7 @@ session_start();
 require_once "../components/db_connect.php";
 
 if (!isset($_SESSION["user_id"]) || !isset($_GET["id"])) {
-    http_response_code(400);
+    header("Location: index.php");
     exit();
 }
 
